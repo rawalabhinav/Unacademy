d323f12397fe785a8534f8476877c71bfd73ca1d
Abhinav Rawal <77731025+rawalabhinav@users.noreply.github.com>
2021-03-09 19:13:33 +0530

    Update MAPINT.py

--- Assignment 11/MAPINT.py
+++ Assignment 11/MAPINT.py
@@ -1,26 +1,52 @@
-allpoints, allends = set(),set()
-endpoints = {}
+endsh, endsv = {}, {}
+horizontal,vertical = set(),set()
 
 for segment in range(int(input())) :
     a,b,name,d = input().split()
-    allpoints.add(a),allpoints.add(b)
-    if name in endpoints :
-        start,end = endpoints[name]
-        if d == "E" or d == "S" :
-            if start == b:
-                endpoints[name][0] = a
-            else :
-                endpoints[name][-1] = b
-        elif start == a :
-            endpoints[name][0] = b
-        else :
-            endpoints[name][-1] = a
-    else :
-        if d == "E" or d == "S" :
-            endpoints[name] = [a,b]
-        else :
-            endpoints[name] = [b,a]
 
-for ends in endpoints.values() : 
-    allends.update(ends)
-print(len(allpoints - allends))
+    if name in endsh.keys()  :
+        horizontal.update([a,b])
+        start, end = endsh[name]
+        if d == "E" :
+            if start == b:
+                endsh[name][0] = a
+            else :
+                endsh[name][-1] = b
+        elif start == a :
+            endsh[name][0] = b
+        else :
+            endsh[name][-1] = a
+
+    elif name in endsv.keys()  :
+        vertical.update([a,b])
+        start, end = endsv[name]
+        if d == "S" :
+            if start == b:
+                endsv[name][0] = a
+            else :
+                endsv[name][-1] = b
+        elif start == a :
+            endsv[name][0] = b
+        else :
+            endsv[name][-1] = a
+    else :
+        if d == "E" : 
+            horizontal.update([a,b])
+            endsh[name] = [a,b]
+        elif d == "W" : 
+            horizontal.update([a,b])
+            endsh[name] = [b,a]
+        elif d == "S" : 
+            vertical.update([a,b])
+            endsv[name] = [a,b]
+        else : 
+            vertical.update([a,b])
+            endsv[name] = [b,a]
+
+temph,tempv = set(),set()
+for ends in endsh.values() : temph.update(ends)
+for ends in endsv.values() : tempv.update(ends)
+horizontal -= temph
+vertical -= tempv
+
+print(len(horizontal&vertical))
